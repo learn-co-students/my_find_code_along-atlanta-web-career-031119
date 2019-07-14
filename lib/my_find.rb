f88925cedce48029be69a5d0f2b38253b7610ef1
github username <github email address>
2019-07-14 17:52:37 -0400

    Done.

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,5 +1,12 @@
-require 'pry'
+require 'pry';
 
 def my_find(collection)
-
+    i=0
+    while i<collection.length
+        if yield(collection[i])
+            return collection[i]
+        else
+            i=i+1
+        end
+    end
 end
